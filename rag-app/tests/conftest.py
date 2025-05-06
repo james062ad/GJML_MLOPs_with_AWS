28803225b5003f991f00f98d9430d76363368646
skok007 <32519240+skok007@users.noreply.github.com>
2025-05-06 13:54:14 +0000

    re=test

--- rag-app/tests/conftest.py
+++ rag-app/tests/conftest.py
@@ -21,17 +21,18 @@ class MockOpenAIClient:
         self.chat = self.Chat()
     
     class Chat:
-        def completions(self):
-            return self
+        def __init__(self):
+            self.completions = self.Completions()
         
-        def create(self, **kwargs):
-            return {
-                "choices": [{
-                    "message": {
-                        "content": "Here is information about perovskites: They are used in solar cells."
-                    }
-                }]
-            }
+        class Completions:
+            def create(self, **kwargs):
+                return {
+                    "choices": [{
+                        "message": {
+                            "content": "Here is information about perovskites: They are used in solar cells."
+                        }
+                    }]
+                }
 
 # Patch the module-level client in generation_service.py
 openai_patcher = patch("server.src.services.generation_service.openai_client", MockOpenAIClient())
